bc4a5f60fd08933b96ba796feb4e432940017071
gerd.mucha <gerd.mucha@googlemail.com>
2014-10-17 09:53:25 +0000

    eeee

--- wicket/userComponents/src/test/java/de/javademo/TestHomePage.java
+++ wicket/userComponents/src/test/java/de/javademo/TestHomePage.java
@@ -7,7 +7,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 /**
- * Simple test using the WicketTester ttt   uuu
+ * Simple test using the WicketTester ttt eee
  */
 public class TestHomePage
 {
