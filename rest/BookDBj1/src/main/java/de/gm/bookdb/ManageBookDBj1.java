a84003ae38c84e0a0c5f98da1cde050c44d211e9
gerd.mucha <gerd.mucha@googlemail.com>
2014-12-29 10:22:22 +0000

    -Post funktioniert -Java-Client mit Post funktioniert

--- rest/BookDBj1/src/main/java/de/gm/bookdb/ManageBookDBj1.java
+++ rest/BookDBj1/src/main/java/de/gm/bookdb/ManageBookDBj1.java
@@ -1,10 +1,14 @@
 package de.gm.bookdb;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
 
 
 @Path("/jsonServices")
@@ -18,6 +22,16 @@ public class ManageBookDBj1 {
 		Book st = new Book( "Perl Hacks", "chromatic", Integer.valueOf(id) );
 
 		return st;
+	}
+	
+	@POST
+	@Path("/send")
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response consumeJSON( Book book ) {
+
+		String output = book.toString();
+
+		return Response.status(200).entity(output).build();
 	}
 
 }
