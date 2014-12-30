75bfa0958f11208d566d49d6348aa78210361965
gerd.mucha <gerd.mucha@googlemail.com>
2014-12-30 11:20:50 +0000

    persistenten Hash eingefügt mit FileHashMap

--- rest/BookDBj1/src/main/java/de/gm/bookdb/ManageBookDBj1.java
+++ rest/BookDBj1/src/main/java/de/gm/bookdb/ManageBookDBj1.java
@@ -1,5 +1,8 @@
 package de.gm.bookdb;
 
+import java.io.IOException;
+import java.util.Map;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
@@ -9,11 +12,31 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.clapper.util.misc.FileHashMap;
+
 
 
 @Path("/jsonServices")
 public class ManageBookDBj1 {
+	
+	public ManageBookDBj1() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
+	Map bookDir;
+	
+
+	public ManageBookDBj1(Map bookDir) {
+		super();
+		try {
+			this.bookDir = new FileHashMap("/local/book_dir.map");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
 	@GET
 	@Path("/print/{id}")
 	@Produces(MediaType.APPLICATION_JSON)
